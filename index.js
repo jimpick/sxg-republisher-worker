5dd0b1a1a8d57c5afd58f04b12f733d78d6caef3
Jim Pick <jim@backblend.com>
2019-05-28 18:18:56 -0700

    Add GitHub OAuth login

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const AWS = require('aws-sdk')
 const delay = require('delay')
+const oauthPlugin = require('fastify-oauth2')
 
 require('dotenv').config()
 
@@ -11,9 +12,33 @@ fastify.register(require('fastify-static'), {
   root: path.join(__dirname, 'public')
 })
 
+fastify.register(oauthPlugin, {
+  // FIXME: GitHub
+  name: 'githubOAuth2',
+  credentials: {
+    client: {
+      id: '72308d8f277638d06b63',
+      secret: '77b47511697c2a41cfd7a6a1389dd5da7cdb3c77'
+    },
+    auth: oauthPlugin.GITHUB_CONFIGURATION
+  },
+  // register a fastify url to start the redirect flow
+  startRedirectPath: '/login/github',
+  // facebook redirect here after the user login
+  callbackUri: 'http://localhost:13023/login/github/callback'
+})
+
 fastify.post('/api/publish', (req, reply) => {
   console.log('Publish request:', req.body)
   reply.send({ success: true })
+})
+
+fastify.get('/login/github/callback', async function (request, reply) {
+  const result = await this.getAccessTokenFromAuthorizationCodeFlow(request)
+
+  console.log(result.access_token)
+
+  reply.send({ access_token: result.access_token })
 })
 
 fastify.listen(13023, (err, address) => {
